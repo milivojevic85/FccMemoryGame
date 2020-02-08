7e487c52c4472ccad7618c3345f9f3750cc9e844
milivojevic85 <misagrf@yahoo.com>
2020-02-08 22:51:14 +0100

    sLocking Board3, n h i

--- scripts.js
+++ scripts.js
@@ -2,18 +2,22 @@ const cards = document.querySelectorAll('.memoryCard');
 
 let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard; let secondCard;
+let firstCard, secondCard;
 
 function flipCard() {
 	if (lockBoard) return;
 	if (this === firstCard) return;
+	
 	this.classList.add('flip');
+	
 	if (!hasFlippedCard) {
+		// first click
 		hasFlippedCard = true;
 		firstCard = this;
-		return
+		return;
 	}
-	hasFlippedCard = false;
+
+	// second click
 	secondCard = this;
 	
 	checkForMatch();
@@ -27,14 +31,21 @@ function checkForMatch() {
 function disableCards() {
 	firstCard.removeEventListener('click', flipCard);
 	secondCard.removeEventListener('click', flipCard);
+	
+	resetBoard();
 }
 function unflipCards() {
 	lockBoard = true;
 	setTimeout(() => {
 		firstCard.classList.remove('flip');
 		secondCard.classList.remove('flip');
-		lockBoard = false;
+		
+		resetBoard();
 	}, 1500);
+}
+function resetBoard() {
+	[hasFlippedCard, lockBoard] = [false, false];
+	[firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
