440ca9dd7b9bbed01621082b2e418902ac10b731
milivojevic85 <misagrf@yahoo.com>
2020-02-08 21:37:54 +0100

    Store C1

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 const cards = document.querySelectorAll('.memoryCard');
 
+let hasFlippedCard = false;
+let firstCard; let secondCard;
+
 function flipCard() {
-	this.classList.toggle('flip');
+	this.classList.add('flip');
+	if (!hasFlippedCard) {
+		// first click
+		hasFlippedCard = true;
+		firstCard = this;
+		
+		console.log({hasFlippedCard, firstCard});
+	}
 }
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
